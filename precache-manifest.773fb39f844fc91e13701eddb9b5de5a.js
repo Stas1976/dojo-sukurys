a2242d6f159f34d2babccb723463b78d39a240f4
Stanislavas Goldyckis <stanislavas.goldyckis@yahoo.com>
2020-06-24 12:16:10 +0300

    Updates

---
+++ precache-manifest.773fb39f844fc91e13701eddb9b5de5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f1801a5ddba643e020388cf60b099d",
+    "url": "/dojo-sukurys/index.html"
+  },
+  {
+    "revision": "7d87a2db4a7909b338fe",
+    "url": "/dojo-sukurys/static/css/main.7a02b1fe.chunk.css"
+  },
+  {
+    "revision": "590fd222c358a9a4685d",
+    "url": "/dojo-sukurys/static/js/2.87e3d405.chunk.js"
+  },
+  {
+    "revision": "8b8fc115f828537022995799a8358a53",
+    "url": "/dojo-sukurys/static/js/2.87e3d405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d87a2db4a7909b338fe",
+    "url": "/dojo-sukurys/static/js/main.a3f3f6bb.chunk.js"
+  },
+  {
+    "revision": "c83b4bf92aa26c7df166",
+    "url": "/dojo-sukurys/static/js/runtime-main.9443f614.js"
+  }
+]);
